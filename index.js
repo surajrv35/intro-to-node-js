c20fd4d0b1f82921686eb3d2496ee187cf4c80f6
Suraj Vadvadgi <surajrv35@gmail.com>
2020-06-06 15:18:34 -0500

    Into to Node.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// const fs = require('fs');
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+var superheroes = require("superheroes");
+var mysuperheroes = superheroes.random();
+console.log(mysuperheroes);
+
+var supervillains = require("supervillains");
+var mysupervillains = supervillains.random();
+console.log(mysupervillains);
